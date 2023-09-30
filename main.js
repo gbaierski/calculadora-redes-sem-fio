53fa065e50b45c28877756d08e890b50d6b68207
gbaierski <gustavobaierski@gmail.com>
2023-09-30 00:09:47 -0300

    feat: Criando conversões de mW para dBm e dBm para mW

--- main.js
+++ main.js
@@ -32,13 +32,11 @@ function shannon() {
     }
 
     var resultado = larguraBandaHz * Math.log2(1 + Math.pow(10, sinalRuidoDb / 10));
-
     document.getElementById('shannon-result').innerHTML = resultado.toFixed(2) + ' bps'
 }
 
 // Função para determinar a Taxa de Nyquist
 function nyquist() {
-
     let larguraBandaHz = document.getElementById('nyquist-larguraBandaHz').value;
     let modulacaoMultinivel = document.getElementById('nyquist-modulacaoMultinivel').value;
 
@@ -48,18 +46,33 @@ function nyquist() {
     }
 
     var resultado = 2 * larguraBandaHz * modulacaoMultinivel;
-
     document.getElementById('nyquist-result').innerHTML = resultado.toFixed(2) + ' bps'
 }
 
 // Função para realizar a conversão de mW para dBm
-function mwToDbm(potenciaMW) {
-    return 10 * Math.log10(potenciaMW);
+function mwToDbm() {
+    let potenciaMW = document.getElementById('mwToDbm-potenciaMW').value;
+
+    if(!potenciaMW) {
+        alert('Por favor, preencha todos os parâmetros!');
+        return;
+    }
+
+    var resultado = 10 * Math.log10(potenciaMW);
+    document.getElementById('mwToDbm-result').innerHTML = resultado.toFixed(2) + ' dBm'
 }
 
 // Função para efetuar a conversão de dBm para mW
-function dbmToMw(potenciaDBm) {
-    return Math.pow(10, potenciaDBm / 10);
+function dbmToMw() {
+    let potenciaDBm = document.getElementById('dbmToMw-potenciaDBm').value;
+
+    if(!potenciaDBm) {
+        alert('Por favor, preencha todos os parâmetros!');
+        return;
+    }
+
+    var resultado = Math.pow(10, potenciaDBm / 10);
+    document.getElementById('dbmToMw-result').innerHTML = resultado.toFixed(2) + ' mW'
 }
 
 // Função para calcular a EIRP (Effective Isotropic Radiated Power)
